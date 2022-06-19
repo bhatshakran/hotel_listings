cd7ff1d778965e6f5f47fabca93e87ea1ec5c404
Bhat Shaqran <bhatshakran@gmail.com>
2022-06-19 12:28:50 +0530

    Initial setup done

---
+++ server/src/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res) => res.send("Hello World"));
+app.listen(port);
+
+console.log(`[app]: http://localhost:${port}`);
