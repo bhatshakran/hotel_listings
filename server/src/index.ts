f6f8e63dee048fad75a2383746d51b9a0f422c9f
Bhat Shaqran <bhatshakran@gmail.com>
2022-06-19 13:02:51 +0530

    Eslint integrated for better code structure/formatting

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,10 @@ import express from "express";
 const app = express();
 const port = 4000;
 
-app.get("/", (req, res) => res.send("Hello World"));
+const one = 1;
+const two = 2;
+
+app.get("/", (req, res) => res.send(`1 + 2 =${one + two}`));
 app.listen(port);
 
 console.log(`[app]: http://localhost:${port}`);
