727ee5f021644b43b498a726067cf4d55ffb5d37
Bhat Shaqran <bhatshakran@gmail.com>
2022-06-20 10:45:52 +0530

    created express server and integrated with apollo server, also created typedefs and resolvers

---
+++ server/src/graphql/resolvers.ts
@@ -0,0 +1,21 @@
+// import { IResolvers }  from 'graphql-tools'
+import { listings } from "../listings";
+
+export const resolvers = {
+  Query: {
+    listings: () => {
+      return listings;
+    },
+  },
+  Mutation: {
+    deleteListing: (_root: undefined, { id }: { id: string }) => {
+      for (let i = 0; i < listings.length; i++) {
+        if (listings[i].id == id) {
+          return listings.splice(i, 1)[0];
+        }
+      }
+
+      throw new Error("Failed to delete listing");
+    },
+  },
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,25 @@
+import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 import express from "express";
+import http from "http";
+import { typeDefs, resolvers } from "./graphql";
 
-const app = express();
-const port = 4000;
+const startApolloServer = async () => {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+    cache: "bounded",
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await server.start();
+  server.applyMiddleware({ app, path: "/api" });
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: 4000 }, resolve)
+  );
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+};
 
-const one = 1;
-const two = 2;
-
-app.get("/", (req, res) => res.send(`1 + 2 =${one + two}`));
-app.listen(port);
-
-console.log(`[app]: http://localhost:${port}`);
+startApolloServer();
