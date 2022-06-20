0051a70893d8b836b7056647de704d78b5cd886b
Bhat Shaqran <bhatshakran@gmail.com>
2022-06-20 11:41:29 +0530

    Connected to mongodb

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,6 +1,6 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const uri = `mongodb+srv://bhatshakran:${process.env.password}@cluster0.dt6yo.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://bhatshakran:${process.env.DB_PASSWORD}@cluster0.dt6yo.mongodb.net/?retryWrites=true&w=majority`;
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -10,8 +10,7 @@ export const connectDatabase = async () => {
   const client = new MongoClient(uri, options);
 
   const db = client.db("main");
-
   return {
-    listings: db.collection("tlistings"),
+    listings: db.collection("listings"),
   };
 };
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,21 @@
+import "dotenv/config";
 import { ApolloServer } from "apollo-server-express";
 import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 import express from "express";
 import http from "http";
 import { typeDefs, resolvers } from "./graphql";
+import { connectDatabase } from "./database";
 
 const startApolloServer = async () => {
+  const db = await connectDatabase();
   const app = express();
   const httpServer = http.createServer(app);
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context: () => {
+      db;
+    },
     csrfPrevention: true,
     cache: "bounded",
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
@@ -20,6 +26,7 @@ const startApolloServer = async () => {
     httpServer.listen({ port: 4000 }, resolve)
   );
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+  console.log(await db.listings.find({}).toArray());
 };
 
 startApolloServer();
